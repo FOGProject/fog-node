675fcf747cc7d5d6fe9514cfb3747d614ae768f5
Tom Elliott <tommygunsster@gmail.com>
2020-04-22 07:18:21 -0500

    Remove models.js from git, update schema to properly define setting value as a json

--- tools/setup/lib/schema.js
+++ tools/setup/lib/schema.js
@@ -7,6 +7,12 @@ var adminRole = {
   },
   adminUser = {
     username: 'Administrator',
+  },
+  schema = {
+    name: 'schema',
+    value: {
+      revision: 0
+    }
   };
 
 module.exports = {
@@ -23,14 +29,28 @@ module.exports = {
     adminUser.password = adminPassword;
     sails.load(cfg, async (err) => {
       if (err) return next(err);
-      await sails.models.setting.findOrCreate({name: 'schema'}, {name: 'schema', value: `{"revision": ${cfg.schema}}`}, async (err, setting) => {
+      if (!cfg.schema) cfg.schema = 1;
+      schema.value.revision = cfg.schema;
+      await sails.models.setting.findOrCreate({name: schema.name}, schema, async (err, setting) => {
         if (err) return next(err);
+        schema.id = setting.id;
+        await sails.models.setting.updateOne({id: setting.id}, schema, async (err, setting) => {
+          if (err) return next(err);
+        });
         await sails.models.role.findOrCreate({name: adminRole.name}, adminRole, async (err, role) => {
           if (err) return next(err);
+          adminRole.id = role.id;
+          await sails.models.role.updateOne({id: role.id}, adminRole, async (err, role) => {
+            if (err) return next(err);
+          });
           adminUser.roles = [role.id];
           await sails.models.user.findOrCreate({username: adminUser.username}, adminUser, async (err, user) => {
             if (err) return next(err);
-            next();
+            adminUser.id = user.id;
+            await sails.models.user.updateOne({id: user.id}, adminUser, async (err, user) => {
+              if (err) return next(err);
+              next();
+            });
           });
         });
       });
