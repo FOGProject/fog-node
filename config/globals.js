bd021e7f92f001043412feb7d5c9e57b3a3bf80e
Tom Elliott <tommygunsster@gmail.com>
2020-04-27 09:52:22 -0500

    Remove printer from menu item as it should be a plugin item

--- config/globals.js
+++ config/globals.js
@@ -190,26 +190,6 @@ module.exports.globals = {
       ],
     },
     {
-      text: 'Printer',
-      plural: 'Printers',
-      link: '/printers',
-      icon: 'fa-print',
-      subLinks: [
-        {
-          link: '/printers/create',
-          text: 'Create New Printer'
-        },
-        {
-          link: '/printers/export',
-          text: 'Export Printers'
-        },
-        {
-          link: '/printers/import',
-          text: 'Import Printers'
-        }
-      ],
-    },
-    {
       text: 'Module',
       plural: 'Modules',
       link: '/modules',
